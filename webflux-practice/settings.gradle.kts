9ae109dafa906d3e1f6cdf80017c45451734f7e9
99winnmin <ojysep9987@gmail.com>
2023-11-06 16:58:17 +0900

    :tada: Feat : WebFlux init setting

---
+++ webflux-practice/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+
+        maven { setUrl("https://repo.spring.io/milestone") } // Spring milestones
+        maven { setUrl("https://repo.spring.io/snapshot") } // Spring snapshots
+    }
+
+    plugins {
+        id("de.fayard.refreshVersions") version "0.51.0"
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions")
+}
+
+rootProject.name = "webflux-practice"
+
+include("practice:completable-future")
+include("practice:completable-future")
+findProject(":practice:completable-future")?.name = "completable-future"

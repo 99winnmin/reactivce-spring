0d2f84b2e4a0b2550ef1ffd4461b32612c0f96dd
99winnmin <ojysep9987@gmail.com>
2023-11-22 18:53:30 +0900

    :sparkles: Feat : Pipeline 예시 주석처리

--- kotlin-coroutines/src/main/kotlin/coroutines/channel/PipeLineEx.kt
+++ kotlin-coroutines/src/main/kotlin/coroutines/channel/PipeLineEx.kt
@@ -1,28 +1,28 @@
-package coroutines.channel
-
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.coroutineScope
-
-suspend fun CoroutineScope.serveOrders(
-    orders: ReceiveChannel<Order>,
-    baristaName: String
-): ReceiveChannel<CoffeeResult> = produce {
-    for (order in orders) {
-        val coffee = prepareCoffer(order.type)
-        send(
-            CoffeeResult(
-                coffee = coffee,
-                customer = order.customer,
-                baristaName = baristaName
-            )
-        )
-    }
-}
-
-val coffeeResult = fanIn(
-    serveOrders(orders, "A"),
-    serveOrders(orders, "A"),
-    serveOrders(orders, "A"),
-)
+//package coroutines.channel
+//
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.channels.ReceiveChannel
+//import kotlinx.coroutines.channels.produce
+//import kotlinx.coroutines.coroutineScope
+//
+//suspend fun CoroutineScope.serveOrders(
+//    orders: ReceiveChannel<Order>,
+//    baristaName: String
+//): ReceiveChannel<CoffeeResult> = produce {
+//    for (order in orders) {
+//        val coffee = prepareCoffer(order.type)
+//        send(
+//            CoffeeResult(
+//                coffee = coffee,
+//                customer = order.customer,
+//                baristaName = baristaName
+//            )
+//        )
+//    }
+//}
+//
+//val coffeeResult = fanIn(
+//    serveOrders(orders, "A"),
+//    serveOrders(orders, "A"),
+//    serveOrders(orders, "A"),
+//)

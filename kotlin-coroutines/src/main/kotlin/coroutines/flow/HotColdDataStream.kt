382f6681d751a276c4e4a9d587c64bebb99e88d5
99winnmin <ojysep9987@gmail.com>
2023-11-26 18:20:02 +0900

    :sparkles: Feat : Hot, Cold 데이터 스트림 차이 이해하기

---
+++ kotlin-coroutines/src/main/kotlin/coroutines/flow/HotColdDataStream.kt
@@ -0,0 +1,27 @@
+package coroutines.flow
+
+fun main() {
+    val l = buildList {
+        repeat(3){
+            add("User$it")
+            println("L: Added User")
+        }
+    }
+
+    val l2 = l.map {
+        println("L: processing")
+        "Processed $it"
+    }
+
+    val s = sequence {
+        repeat(3) {
+            yield("User$it")
+            println("S: Added User")
+        }
+    }
+
+    val s2 = s.map {
+        println("S: Processing")
+        "Processed $it"
+    }
+}

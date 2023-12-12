6af18ac17de999bcf0b67cc8e942fb67b4899cbc
99winnmin <ojysep9987@gmail.com>
2023-12-12 19:18:04 +0900

    :sparkles: Feat : Bootstrap echo 서버 실습

---
+++ webflux-practice/practice/spring-webflux/src/main/java/com/example/netty/training/NettyEchoServer.java
@@ -0,0 +1,67 @@
+package com.example.netty.training;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoop;
+import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NettyEchoServer {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        EventLoopGroup parentGroup = new NioEventLoopGroup();
+        EventLoopGroup childGroup = new NioEventLoopGroup(4);
+        EventExecutorGroup eventExecutorGroup = new DefaultEventLoopGroup(4);
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            var server = serverBootstrap
+                .group(parentGroup, childGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<Channel>() {
+                   @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast(
+                            eventExecutorGroup, new LoggingHandler(LogLevel.INFO)
+                        );
+                        ch.pipeline().addLast(
+                            new StringEncoder(),
+                            new StringDecoder(),
+                            new NettyEchoServerHandler()
+                        );
+                    }
+                });
+
+            server.bind(8080).sync()
+                .addListener(new FutureListener<>() {
+                    @Override
+                    public void operationComplete(Future<Void> future) throws Exception {
+                        if (future.isSuccess()) {
+                            log.info("Server bound");
+                        } else {
+                            log.error("Bound attempt failed");
+                        }
+                    }
+                }).channel().closeFuture().sync();
+        } finally {
+            parentGroup.shutdownGracefully();
+            childGroup.shutdownGracefully();
+            eventExecutorGroup.shutdownGracefully();
+        }
+    }
+}
